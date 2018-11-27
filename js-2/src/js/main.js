d3efb911dcbc88c2354802b244844172922972eb
Vitaliy-Dyachuk <shadespiritenator@gmail.com>
2018-11-27 13:35:41 +0200

    Lesson-2

---
+++ js-2/src/js/main.js
@@ -0,0 +1,48 @@
+document.write('<div> Add some new </div>');
+document.write('<br>')
+document.write(`<div> Add some new </div>`);
+
+console.log('Hello console!!!  555');
+
+
+var x = 777;
+console.log(x);
+
+var a = 'Some text';
+console.log(a)
+
+var b = true;
+console.log(b);
+
+
+let a1 = 2020;
+const cc = 'I am constant';
+
+console.log(a1, cc);
+
+a1 = 999;
+// !!! cc = 0;
+
+console.log(a1, cc);
+
+
+// var, let, const
+
+let result;
+let y1 = 5;
+let y2 = 6;
+
+result = y1 + y2;  // 5 + 6 => 11
+document.write(result);
+
+
+let u
+console.log(u)
+
+let nn = 'text' * 4;
+console.log(nn)
+
+
+
+
+
